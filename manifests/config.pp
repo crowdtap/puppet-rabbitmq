661161beda851da3c5cf963bf008c90b09abe059
Otavio Nascimento <otanasc@gmail.com>
2016-05-25 12:31:57 +1000

    fixing executable path

--- manifests/config.pp
+++ manifests/config.pp
@@ -10,7 +10,7 @@ class rabbitmq::config {
   $configfile     = "${configdir}/rabbitmq.conf"
   $enabledplugins = "${configdir}/enabled_plugins"
   $datadir        = "${boxen::config::datadir}/rabbitmq"
-  $executable     = "${boxen::config::home}/homebrew/sbin/rabbitmq-server"
+  $executable     = "/usr/local/sbin/rabbitmq-server"
   $logdir         = "${boxen::config::logdir}/rabbitmq"
   $port           = 55672
   $mgmt_port      = 15672
